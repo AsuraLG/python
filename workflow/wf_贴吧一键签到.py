a51cc432e7d24676bae90a2ef049d3a1560e29f5
AsuraLG <1006199824@qq.com>
2018-09-12 13:27:59 +0800

    更新了小工具和workflow里的贴吧签到脚本
    Signed-off-by: AsuraLG <1006199824@qq.com>

--- workflow/wf_贴吧一键签到.py
+++ workflow/wf_贴吧一键签到.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# -*- coding: utf-8 -*-
 
 import requests
 import re
@@ -8,28 +8,39 @@ import datetime
 import sys
 import ast
 
+# 获取当前时间戳
 starttime = datetime.datetime.now()
+
+# 从参数获取BDUSS
 rawInput = sys.argv.__str__()
 listInput = ast.literal_eval(rawInput)
 BDUSS = listInput[1]
-#print BDUSS
-infoUrl = "https://tieba.baidu.com/?page=like";
-cookies = {"BDUSS" : BDUSS}
-headers = {'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X; zh-CN) AppleWebKit/537.51.1 (KHTML, like Gecko) Mobile/14B100 UCBrowser/10.7.5.650 Mobile'}
-response = requests.get(infoUrl, cookies = cookies, headers = headers)
 
-matcher = re.findall(r'(?<=<div class="forumTile_name">).*(?=</div>)', response.text.encode('UTF_8'))
-for i in range(len(matcher)):
-	match = re.search(r'<div class="forumTile_name">' + matcher[i] + r'</div>\r\n\t{1,50}</div><div class="(.*?)">', response.text)
+# 获取包含全部关注的贴吧名称的网页
+nameUrl = 'https://tieba.baidu.com/?page=like';
+cookies = {'BDUSS' : BDUSS}
+headers = {'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X; zh-CN) AppleWebKit/537.51.1 (KHTML, like Gecko) Mobile/14B100 UCBrowser/10.7.5.650 Mobile'}
+response = requests.get(nameUrl, cookies = cookies, headers = headers)
+
+# 获取tbs的值
+tbs = re.search(r'tbs : "(.*?)"', response.text).group(1)
+
+# 获取全部关注的贴吧名称记录在列表names中
+names = re.findall(r'(?<=<div class="forumTile_name ">).*?(?=</div>)', response.text.encode('UTF_8'))
+
+# 遍历所有关注的贴吧名称，若没有签到则签到
+for i in range(len(names)):
+	match = re.search(r'<div class="forumTile_name ">' + names[i] + r'</div></div><div class="(.*?)">', response.text.encode('UTF_8'))
 	if match and 'forumTile_signforumTilesign global_icon' == match.group(1):
 		continue
 		
-	signUrl = "http://tieba.baidu.com/mo/m/sign?tbs=8381d2143c59d0c91491024593&fid=552164&kw=" + matcher[i]
+	signUrl = 'http://tieba.baidu.com/mo/m/sign?tbs=' + str(tbs) + '&fid=552164&kw=' + names[i]
 	requests.post(signUrl, cookies = cookies, headers = headers)
-	#print matcher[i]
-	
+
+# 获取当前时间戳，计算签到用时
 endtime = datetime.datetime.now()
 seconds = (endtime - starttime).seconds
 
-clipboard.set('已签到贴吧' + str(len(matcher)) + '个成功，用时' + str(seconds) + '秒')
+# 返回workflow
+clipboard.set('已签到贴吧' + str(len(names)) + '个成功，用时' + str(seconds) + '秒')
 webbrowser.open('workflow://')
--- 小工具/贴吧一键签到.py
+++ 小工具/贴吧一键签到.py
@@ -2,30 +2,38 @@
 
 import requests
 import re
-#import webbrowser
-#import clipboard
 import datetime
 
-starttime=datetime.datetime.now()
-BDUSS = "hyS0R3akxrT1VUZG5BczNONmNnTXphV2MydVV6bXpzck1PVTh0VkYtVnFDN3RhQVFBQUFBJCQAAAAAAAAAAAEAAABd3s80bGc4MDIzbW91c2UAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGp-k1pqfpNaR2";
-infoUrl = "https://tieba.baidu.com/?page=like";
-cookies = {"BDUSS" : BDUSS}
-headers = {'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X; zh-CN) AppleWebKit/537.51.1 (KHTML, like Gecko) Mobile/14B100 UCBrowser/10.7.5.650 Mobile'}
-response = requests.get(infoUrl, cookies = cookies, headers = headers)
+# 已签到贴吧计数器
+counter = 0
 
-matcher = re.findall(r'(?<=<div class="forumTile_name">).*(?=</div>)', response.text)
-for i in range(len(matcher)):
-	match = re.search(r'<div class="forumTile_name">' + matcher[i] + r'</div>\r\n\t{1,50}</div><div class="(.*?)">', response.text)
+# 获取当前时间戳
+starttime=datetime.datetime.now()
+
+# 获取包含全部关注的贴吧名称的网页
+BDUSS = 'lhWWXJscWlBNXdDNGVOT1JjTXdjSU1BdEdrb1dsRkQ3Ni0wOVl3anhDblZmcjliQUFBQUFBJCQAAAAAAAAAAAEAAABd3s80bGc4MDIzbW91c2UAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANXxl1vV8Zdbb';
+nameUrl = 'https://tieba.baidu.com/?page=like';
+cookies = {'BDUSS' : BDUSS}
+headers = {'User-Agent' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X; zh-CN) AppleWebKit/537.51.1 (KHTML, like Gecko) Mobile/14B100 UCBrowser/10.7.5.650 Mobile'}
+response = requests.get(nameUrl, cookies = cookies, headers = headers)
+
+# 获取tbs的值
+tbs = re.search(r'tbs : "(.*?)"', response.text).group(1)
+
+# 获取全部关注的贴吧名称记录在列表names中
+names = re.findall(r'(?<=<div class="forumTile_name ">).*?(?=</div>)', response.text)
+
+# 遍历所有关注的贴吧名称，若没有签到则签到
+for i in range(len(names)):
+	match = re.search(r'<div class="forumTile_name ">' + names[i] + r'</div></div><div class="(.*?)">', response.text)
 	if match and ('forumTile_sign global_icon' == match.group(1)):
 		continue
 	
-	signUrl = "http://tieba.baidu.com/mo/m/sign?tbs=8381d2143c59d0c91491024593&fid=552164&kw=" + matcher[i]
+	signUrl = 'http://tieba.baidu.com/mo/m/sign?tbs=' + str(tbs) + '&fid=552164&kw=' + names[i]
 	requests.post(signUrl, cookies = cookies, headers = headers)
-	#print matcher[i]
-
+	counter+=1
+	
+# 获取当前时间戳，计算签到用时
 endtime = datetime.datetime.now()
 seconds = (endtime - starttime).seconds
-print u'用时：' + str(seconds) + u'秒'
-
-#clipboard.set('已签到贴吧' + str(len(matcher)) + '个成功，用时' + str(seconds) + '秒')
-#webbrowser.open('workflow://')
+print u'用时 ' + str(seconds) + u' 秒，签到 ' + str(counter) + u' 个贴吧'
