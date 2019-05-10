1bc6b228f81f11f154b0f34ad9e93829ab1ebc98
AsuraLG <1006199824@qq.com>
2019-05-10 14:38:24 +0800

    针对百度贴吧网页更改而做的更新
    Signed-off-by: AsuraLG <1006199824@qq.com>

--- 小工具/贴吧一键签到.py
+++ 小工具/贴吧一键签到.py
@@ -35,7 +35,7 @@ nameUrl = 'http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/m?tn=bdFBW&tab=fav
 sTime = datetime.now()
 nameResp = session.get(nameUrl, cookies = commonCookies, headers = commonHeaders)
 eTime = datetime.now()
-print u'获取包含全部关注的贴吧名称的网页用时 ' + str((eTime - sTime).microseconds) + u' 毫秒'
+#print u'获取包含全部关注的贴吧名称的网页用时 ' + str((eTime - sTime).microseconds) + u' 毫秒'
 #print nameResp.text
 
 # 从网页中生成包含全部贴吧名称的列表
@@ -45,7 +45,7 @@ nameDoc = BeautifulSoup(nameResp.text, 'lxml', parse_only = only_table_class_tb)
 #print nameDoc
 nameList = [(name.text, name.get('href')[5:]) for name in nameDoc.select('a')] # name.text是可读编码的贴吧名称，name.get('href')[5:]是url编码的贴吧名称
 eTime = datetime.now()
-print u'从网页中生成包含全部贴吧名称的列表用时 ' + str((eTime - sTime).microseconds) + u' 毫秒'
+#print u'从网页中生成包含全部贴吧名称的列表用时 ' + str((eTime - sTime).microseconds) + u' 毫秒'
 #print nameList
 
 # 对每一个贴吧，发送tbs查询请求并签到
@@ -53,34 +53,35 @@ for name  in nameList:
 	# 获取每个贴吧的主页，此处换成电脑浏览器的UA才可正常
 	sTime = datetime.now()
 	# 第一种方式
-	#specialHeaders = {
-	#	'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'
-	#}
-	#checkUrl = 'http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/m?kw=' + name[1]
-	#checkResp = session.get(checkUrl, cookies = commonCookies, headers = specialHeaders)
+	specialHeaders = {
+		'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'
+	}
+	checkUrl = 'http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/m?kw=' + name[1]
+	checkResp = session.get(checkUrl, cookies = commonCookies, headers = specialHeaders)
 	
 	# 第二种方式
-	checkUrl = 'http://tieba.baidu.com/f?ie=utf-8&kw=' + name[0] + '&fr=search'
-	checkResp = session.get(checkUrl, cookies = commonCookies, headers = commonHeaders)
+	#checkUrl = 'http://tieba.baidu.com/f?ie=utf-8&kw=' + name[0] + '&fr=search'
+	#checkResp = session.get(checkUrl, cookies = commonCookies, headers = commonHeaders)
 	
 	#print checkUrl
 	eTime = datetime.now()
-	print u'获取贴吧的主页用时 ' + str((eTime - sTime).microseconds) + u' 毫秒'
+	#print u'获取贴吧的主页用时 ' + str((eTime - sTime).microseconds) + u' 毫秒'
 	#print checkResp.text
+	#raise RuntimeError('test')
 	
 	# 提取tbs
 	sTime = datetime.now()
 	# 第一种方式
-	#htmlDoc = BeautifulSoup(checkResp.text, 'lxml')
+	htmlDoc = BeautifulSoup(checkResp.text, 'lxml')
 	#print htmlDoc
-	#tbs = htmlDoc.select('input[name="tbs"]')[0].get('value')
+	tbs = htmlDoc.select('input[name="tbs"]')[0].get('value')
 	
 	# 第二种方式
-	tbs = re.search(r'"tbs":"(.*?)"', checkResp.text).group(1)
+	#tbs = re.search(r'"tbs":"(.*?)"', checkResp.text).group(1)
 	#print tbs
 	
 	eTime = datetime.now()
-	print u'提取tbs用时 ' + str((eTime - sTime).microseconds) + u' 毫秒'
+	#print u'提取tbs用时 ' + str((eTime - sTime).microseconds) + u' 毫秒'
 	
 	# 实施签到
 	signData = {
